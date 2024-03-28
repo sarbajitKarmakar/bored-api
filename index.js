0e3c93d04d6a83371e8944d39147a8fef9169189
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-03-28 13:23:59 +0530

    featching api done.. Project completed

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com/random";
 
 // 3. Use the public folder for static files.
 app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
-app.get("/" , (req , res) =>{
-    res.render("index.ejs");
+app.get("/" , async (req , res) =>{
+    const response = await axios.get(API_URL);
+    const result = response.data;
+    res.render("index.ejs",{
+        secret : result.secret,
+        user: result.username,
+    });
 })
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
